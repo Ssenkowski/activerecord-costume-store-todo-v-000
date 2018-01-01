f434ff6d8bcd3bb95d1d621fa799ee886d5bd0db
Steven Senkowski <scsenkowski@gmail.com>
2018-01-01 15:15:54 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,6 +2,8 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.0]
   def change
     create_table :haunted_houses do |t|
       t.string :name
+
+      t.timestamps null: false
     end
   end
 end
