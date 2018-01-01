99fda835cce8ec6a73ba15293fb087daf83b7532
Steven Senkowski <scsenkowski@gmail.com>
2018-01-01 15:04:44 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -2,7 +2,11 @@ class CreateCostumes < ActiveRecord::Migration
   def change
     create_table :costumes do |t|
       t.string :name
+      t.string :size
+      t.string :image_url
+      t.float :price
 
-      t.timestamps
+      t.timestamps null: false
+    end
   end
 end
