1126f0a58fc0f7c815c7ef94c27c263d4a8eb37a
Steven Senkowski <scsenkowski@gmail.com>
2018-01-01 15:16:52 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 3) do
 
   create_table "haunted_houses", force: :cascade do |t|
     t.string "name"
+    t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
